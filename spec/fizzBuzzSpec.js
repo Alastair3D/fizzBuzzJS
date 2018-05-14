355f1a36dbf897a9470af16dc31db10cb9d3c762
William Powell <william.powell92@gmail.com>
2018-05-14 15:36:32 +0100

    passed test for printing one, failing test for printing 2

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,11 +2,15 @@ describe("fizzBuzz", function() {
   var fizzBuzz;
 
   beforeEach(function() {
-    fizzBuzz = new fizzBuzz();
+    fizzBuzz = new FizzBuzz();
   });
 
   it("should be able to print the number one", function() {
     expect(fizzBuzz.play(1)).toEqual(1);
+  });
+
+  it("should be able to print the number two", function() {
+    expect(fizzBuzz.play(2).toEqual(2));
   });
 
 });
---
+++ src/fizzBuzz.js
@@ -0,0 +1,6 @@
+function FizzBuzz() {
+}
+
+FizzBuzz.prototype.play = function(num) {
+  return 1;
+};
